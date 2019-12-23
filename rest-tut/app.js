67b4fadbfb4436130edf45a8a653c44fd63500e2
johnfruscella <johnfruscella@gmail.com>
2019-12-22 20:45:50 -0500

    added post file and edited app file

--- rest-tut/app.js
+++ rest-tut/app.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 //ROUTES
 app.get('/', (req, res) => {
     res.send('We are on home');
 });
 
-app.get('/posts', (req, res) => {
-    res.send('We are on posts');
-});
-
 
 //Connect to Data Base (DB) contains user name an
-mongoose.connect('mongodb+srv://johnf:<password>@cluster0-pyvqf.mongodb.net/test?retryWrites=true&w=majority')
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  };
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    options,
+    () => console.log('Connected to DB')
+);
+
 
 
 //How do we start listening to the server?
